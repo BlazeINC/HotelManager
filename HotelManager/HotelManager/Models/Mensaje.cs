7a6de3b4fb6b0a94a067a3291abe72602ef1516b
amadorm93 <amadorm93@gmail.com>
2020-11-20 00:56:08 -0600

    Creamos la clase Mensaje y agregamos un archivo de navegacion

---
+++ HotelManager/HotelManager/Models/Mensaje.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManager.Models
+{
+    class Mensaje
+    {
+        public string ID { get; set; }
+        public string Msg { get; set; }
+        public int Tipo { get; set; }
+        public string IDHuesped { get; set; }
+        public int Estatus { get; set; }
+        public Mensaje()
+        {
+            ID = string.Empty;
+            Msg = string.Empty;
+            Tipo = 0;
+            IDHuesped = string.Empty;
+            Estatus = 1;
+        }
+
+    }
+}
