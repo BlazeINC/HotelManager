0fdfe72f7b77b6c677770b0f754f677cc8ace9b0
amadorm93 <amadorm93@gmail.com>
2020-11-20 01:26:13 -0600

    Se agregaron las configuraciones de la app para verificar usuarios (falta corregir)

--- HotelManager/HotelManager/App.xaml.cs
+++ HotelManager/HotelManager/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using HotelManager.Models;
+using HotelManager.Views.Forms;
+using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -6,13 +10,21 @@ namespace HotelManager
 {
     public partial class App : Application
     {
+        public static string DatabaseLocation = string.Empty;
+        public static MobileServiceClient client = new MobileServiceClient("https://hotelmanagerwebapp.azurewebsites.net/%22);
+        public static Usuarios sesion = new Usuarios();
+        public static List<Reservacion> reservaciones = new List<Reservacion>();
+        public static List<menus> ord = new List<menus>();
+        public static string BaseImageUrl { get; } = "https://cdn.syncfusion.com/essential-ui-kit-for-xamarin.forms/common/uikitimages/";
         public App()
         {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjU5MjMyQDMxMzgyZTMxMmUzMGFGV0lJclMzN0t6NkxGUkNsOTlRZUhEbkh3U3g2MVIrYzNJQTB1K2hZeGs9");
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new SimpleLoginPage());
         }
 
+      
         protected override void OnStart()
         {
         }
--- HotelManager/HotelManager/MainPage.xaml.cs
+++ HotelManager/HotelManager/MainPage.xaml.cs
@@ -1,18 +1,35 @@
-﻿using System;
+﻿using HotelManager.Views.Forms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.GTKSpecific;
 
 namespace HotelManager
 {
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void BtnLogin_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(TextUsuario.Text) || string.IsNullOrEmpty(TextUsuario.Text))
+            {
+
+            }
+            else
+            {
+
+            }
         }
     }
 }
